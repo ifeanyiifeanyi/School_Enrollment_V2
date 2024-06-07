03a8a25ed1cc3f6eedfc41d525d81a776cd46f77
Ifeanyi Nnaemego <josephnnaemego@gmail.com>
2024-06-07 11:10:16 +0100

    crop img 01

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
+        $checkValidSignature = (config('app.env') === 'production' && str_contains(URL::current(), 'livewire/upload-file'));
+
+        Request::macro('hasValidSignature', function ($absolute = true) use ($checkValidSignature) {
+            if ($checkValidSignature) {
+                return true;
+            }
+            return URL::hasValidSignature($this, $absolute);
+        });
+
+        Request::macro('hasValidRelativeSignature', function ()  use ($checkValidSignature) {
+            if ($checkValidSignature) {
+                return true;
+            }
+            return URL::hasValidSignature($this, $absolute = false);
+        });
+
+        Request::macro('hasValidSignatureWhileIgnoring', function ($ignoreQuery = [], $absolute = true)   use ($checkValidSignature) {
+            if ($checkValidSignature) {
+                return true;
+            }
+            return URL::hasValidSignature($this, $absolute, $ignoreQuery);
+        });
         if (env('APP_ENV') === 'production') {
             $url->forceScheme('https');
         }
