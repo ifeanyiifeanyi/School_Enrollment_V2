90e3778d3e84ca78dc6646070b33f515e55e0f66
Ifeanyi Nnaemego <josephnnaemego@gmail.com>
2024-06-08 04:51:55 +0100

    overhaul 007

--- resources/views/student/layouts/partials/sidebar.blade.php
+++ resources/views/student/layouts/partials/sidebar.blade.php
@@ -17,10 +17,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="pb-3 mt-3 mb-3 user-panel d-flex">
             <div class="image">
-                <img src="{{ empty(auth()->user()->student->passport_photo)
-                    ? 'https://placehold.it/150x100'
-                    : Storage::url(auth()->user()->student->passport_photo) }}"
-                    class="elevation-2" alt="User Image">
+<img src="{{ empty(auth()->user()->student->passport_photo) ? 'https://placehold.it/150x100' : asset(auth()->user()->student->passport_photo) }} "class="elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a wire:navigate href="{{ route('student.profile') }}"
