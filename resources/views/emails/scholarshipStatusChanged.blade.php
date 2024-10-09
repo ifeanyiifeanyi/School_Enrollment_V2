238de2a43216551b48b94f8bdd4b2e403795e048
Ifeanyi Nnaemego <josephnnaemego@gmail.com>
2024-10-09 15:09:34 +0100

    scholar edit

--- resources/views/emails/scholarshipStatusChanged.blade.php
+++ resources/views/emails/scholarshipStatusChanged.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Scholarship Application Status Updated</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
     <style>
         body {
             background-color: #f8f9fa;
